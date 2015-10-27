0b152411593d9c807013aad05b4d837790daf9a8
Kostrov Sergey <a.big.thick.dictionary@gmail.com>
2015-10-27 18:15:56 +0300

    Initial commit

---
+++ GradientDescent/GradientDescent/headers.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+#include <memory>
+#include <thread>
+#include <exception>
+
+#include <string>
+#include <array>
+#include <vector>
+#include <list>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+
+#include "timer.h"
+
+
+using namespace std;
